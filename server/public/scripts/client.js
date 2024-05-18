8f3f24a718b67423aa4ca20ca2f1ef06b36dfb0a
crystal <crystalevenram@gmail.com>
2024-05-18 12:04:38 -0500

    made a plan and created ids and table in index.html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,54 @@
 console.log('JS is sourced!');
+
+// call the function to Display the list of existing songs on page when it loads
+
+
+
+//create a function to GET Todo List from the server
+// function (GetTodos)
+
+function () {
+    
+    axios.get('/').then((response) => {
+        console.log("GET / response", response);
+        (response.data);
+    });
+}
+
+// create a function to append todo list to the dom 
+    //function appendTodosToDom(){}
+
+
+    //loop over each todo and append data to the dom
+
+
+
+
+
+//clear form -- need to clear the input forms 
+    //initialize function clearform(){}
+
+
+
+// POST --- send the users input to the sever 
+    //git initialize function postTodo()
+
+    //create our request body
+
+
+    // send the new todo to the server 
+
+
+
+// PUT -- sending a put request to change completion status on the todo list 
+        // initialize function completionStatus (){}
+
+    // Use axios to send a PUT request to change to complete
+        // Send  & id in URL
+    // For .then, will call the render function.
+
+
+
+
+// Delete-- 
+    // Remove a song from our list
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -4,4 +4,62 @@ const pool = require('../modules/pool');
 
 
 
+
+// recieved clients GET request and repsond with result.rows
+router.get('/', (req, res) => {
+
+ //Query text to send to the database (SELECT)
+        // Using backticks will be easier since queries often have quotes in them
+        // Needs to be the exact SQL that you would write in Postico
+
+    
+            //  let queryText = `SELECT * FROM "todos";`
+
+    // Use pool to make the transaction with the Database 
+
+})
+
+ 
+
+
+
+//POST route:
+router.post('/', (req, res) => {
+    console.log('req.body', req.body);
+
+    
+
+    // ! Use parameterization ✅
+
+
+    // Use the pool to make the transaction
+    // pool.query(queryText, [SOME ARRAY OF PARAMETERS])
+})  
+
+//PUT Route:
+
+router.put('/rank/:id', (req, res) => {
+
+ //Query text to send to the database (SELECT)
+        // Using backticks will be easier since queries often have quotes in them
+        // Needs to be the exact SQL that you would write in Postico
+
+    
+            //  let queryText = `SELECT * FROM "todos";`
+
+    // Use pool to make the transaction with the Database 
+    // Remember: The URL itself will be used to hold some small bit of data. (parameters)
+
+
+})
+
+
+
+
+//DELETE route:
+router.delete('/:id', (req, res) => {
+
+});
+
+
 module.exports = router;
