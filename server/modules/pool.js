67572f3c4ee93a0574a23d6be78516311401b9dc
crystal <crystalevenram@gmail.com>
2024-05-20 16:31:35 -0500

    updated pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,46 @@
-const pg = require('pg')
+// const pg = require('pg')
 
-let databaseName = 'weekend-to-do-app'
+// let databaseName = 'weekend-to-do-app'
+
+// if (process.env.NODE_ENV === 'test') {
+//   databaseName = 'prime_testing'
+// }
+
+// const pool = new pg.Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     database: databaseName,
+//     allowExitOnIdle: true 
+// })
+
+
+const pg = require('pg');
+
+let pool;
+
+let databaseName = 'weekend-to-do-app';
 
 if (process.env.NODE_ENV === 'test') {
-  databaseName = 'prime_testing'
+databaseName = 'prime_testing';
 }
 
-const pool = new pg.Pool({
-    host: 'localhost',
-    port: 5432,
-    database: databaseName,
-    allowExitOnIdle: true 
-})
+// db connection
+if (process.env.DATABASE_URL) {
+pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+} else {
+pool = new pg.Pool({
+  host: 'localhost',
+  port: 5432,
+  database: databaseName, // use the variable set earlier
+  allowExitOnIdle: true,
+});
+}
 
-module.exports = pool
+module.exports = pool;
+
+
